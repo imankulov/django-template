14cf844cc9ddbfc37cc2ffd1def74cdb1720c269
Roman Imankulov <roman@netangels.ru>
2012-09-01 14:43:51 +0600

    Added admin.py sample

---
+++ project_name/core/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+#
+# This is a admin.py bootstrap example
+#
+#from {{ project_name }}.core.models import Foo
+#
+#class FooAdmin(admin.ModelAdmin):
+#    pass
+#
+#admin.site.register(Foo, FooAdmin)
