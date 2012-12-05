dc298292c89dec4284c0a4e0d580140801949b10
Roman Imankulov <roman.imankulov@gmail.com>
2012-12-05 18:07:14 +0300

    Updated .gitignore and urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
